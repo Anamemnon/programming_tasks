64d20df67437313c812a7ef8cca3c63ed87ca689
Dmitry Chekir <chekirdmitry@gmail.com>
2023-08-17 21:53:37 +0300

    feat(leetcode:easy:C#): add "169. Majority Element" task

---
+++ leetcode/Easy/169. Majority Element/Solution/Program.cs
@@ -0,0 +1,25 @@
+﻿_ = Solution.MajorityElement(new int[] { 3, 2, 3 });
+
+public class Solution
+{
+    public static int MajorityElement(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+
+        Dictionary<int, int> counter = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var temp = nums[i];
+
+            if (counter.ContainsKey(temp))
+            {
+                counter[temp]++;
+
+                if (i + 1 > nums.Length / 2 && counter[temp] > nums.Length / 2) return temp;
+            }
+            else counter[temp] = 1;
+        }
+        return 0;
+    }
+}
