a256191ace615a505509f4970bdfd97c887a6c3b
Dmitry Chekir <chekirdmitry@gmail.com>
2024-06-02 21:14:17 +0300

    [~]

--- leetcode/Easy/0001. Two Sum/Python/Solution.py
+++ leetcode/Easy/0001. Two Sum/Python/Solution.py
@@ -4,16 +4,15 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = dict()
-
+        
         for i, v in enumerate(nums):
             key = target - v
             if key in d:
                 return [d[key], i]
-
             d[v] = i
 
 
 if __name__ == '__main__':
-    print(Solution().twoSum(nums = [2,7,11,15], target = 9))
-    print(Solution().twoSum(nums = [3,2,4], target = 6))
-    print(Solution().twoSum(nums = [3,3], target = 6))
+    print(Solution().twoSum(nums = [2,7,11,15], target = 9) == [0, 1])
+    print(Solution().twoSum(nums = [3,2,4], target = 6) == [1, 2])
+    print(Solution().twoSum(nums = [3,3], target = 6) == [0, 1])
